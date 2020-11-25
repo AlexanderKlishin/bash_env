3e4e4fb2fd6cd4f7f2dfb46f1e3fa45015df3816
Alexander.Klishin <alexander.klishin@billing.ru>
2020-11-25 07:54:52 +0300

    build_rtags.sh

--- build_rtags.sh
+++ build_rtags.sh
@@ -2,14 +2,6 @@
 
 SH=$(cd `dirname $0` && pwd)
 
-# check reqirements
-exists_or_exit() {
-    which $1 > /dev/null || { echo "$1 not installed"; exit 1; }
-}
-exists_or_exit g++
-#exists_or_exit llvm-config
-exists_or_exit cmake
-
 HOME=$(cd ~ && pwd)
 INSTALL_PREFIX=$HOME/rtags
 
@@ -35,6 +27,11 @@ COMMON+=" -DCMAKE_BUILD_TYPE=Release"
 if [ $OS = "Ubuntu" ]; then
     cmake .. $COMMON
 elif [ $OS = "RedHat7" ]; then
+    CLANGVER=$(clang++ --version | grep clang | cut -d " " -f 3 | cut -d "." -f 1)
+    if [ "$CLANGVER" -lt 7 ]; then
+        echo "clang++ must be >= 7, current: $CLANGVER"
+        exit 1
+    fi
     if [ ! -d /opt/rh/llvm-toolset-7.0 ]; then
         echo "yum install llvm-toolset-7.0 llvm-toolset-7.0-clang-devel llvm-toolset-7.0-llvm-devel devtoolset-8"
         echo "scl enable devtoolset-8 llvm-toolset-7.0"
@@ -46,48 +43,10 @@ elif [ $OS = "RedHat7" ]; then
     CC=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang \
     cmake .. $COMMON -DLLVM_CONFIG=/opt/rh/llvm-toolset-7.0/root/bin/llvm-config
 else
-    if [ -z "$GCC_HOME" ]; then
-        if [ -d /usr/local/CC/gcc-4.8.5 ]; then
-            GCC_HOME=/usr/local/CC/gcc-4.8.5
-        else
-            GCC_HOME=$(dirname $(dirname $(which g++)))
-        fi
-    fi
-    LLVM_HOME=$HOME/llvm
-    [ -d $LLVM_HOME ] || LLVM_HOME=$(dirname $(dirname $(which llvm-config)))
-
-    echo -e "Using:\n\tgcc:$GCC_HOME\n\tllvm:$LLVM_HOME"
-
-    cmake .. -DLLVM_CONFIG=$LLVM_HOME/bin/llvm-config \
-        -DCMAKE_CXX_COMPILER=$GCC_HOME/bin/clang++ \
-        -DCMAKE_C_COMPILER=$GCC_HOME/bin/clang \
-        $COMMON
+    echo "build $OS not supported"
 fi
 
-make -j 4
+make -j 8
 make install
 cp bin/* $INSTALL_PREFIX/bin
 cp $SH/rdm.sh $INSTALL_PREFIX/bin
-#cp $SH/rc.sh $INSTALL_PREFIX/bin
-
-cd $INSTALL_PREFIX/bin
-
-#rm -f g++ c++ gcc c
-#ln -s gcc-rtags-wrapper.sh g++
-#ln -s gcc-rtags-wrapper.sh c++
-#ln -s gcc-rtags-wrapper.sh gcc
-#ln -s gcc-rtags-wrapper.sh cc
-#ln -s gcc-rtags-wrapper.sh c
-
-#if [ $OS = "Ubuntu" ]; then
-    #:
-#else
-    #rm -f *.impl
-    #mv rc rc.impl
-
-    #cat << EOF > rc
-##!/bin/bash -e
-#LD_LIBRARY_PATH=/opt/rh/llvm-toolset-7/root/usr/lib64 ~/rtags/bin/rc.impl \$@
-#EOF
-        #chmod +x rc
-#fi
