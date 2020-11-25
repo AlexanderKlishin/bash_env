60d4c64a3cb07dca86c6305c185006ae31881af3
Alexander.Klishin <alexander.klishin@billing.ru>
2020-11-25 08:23:16 +0300

    clang 9

--- devtools/devtools.sh
+++ devtools/devtools.sh
@@ -9,7 +9,8 @@ sudo cp nx_scl.repo /etc/yum.repos.d/
 sudo yum install -y devtoolset-8 \
     devtoolset-8-libasan-devel \
     devtoolset-8-libtsan-devel \
-    llvm-toolset-7.0 \
-    llvm-toolset-7.0-clang-devel \
-    llvm-toolset-7.0-llvm-devel \
-    llvm-toolset-7.0-cmake
+    llvm-toolset-9.0 \
+    llvm-toolset-9.0-clang-devel \
+    llvm-toolset-9.0-llvm-devel
+    llvm-toolset-9.0-cmake
+
