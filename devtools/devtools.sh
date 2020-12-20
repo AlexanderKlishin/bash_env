f642bf3e3c7869dd5fc87f7596e7c76112eed6fc
Alexander.Klishin <alexander.klishin@billing.ru>
2020-12-20 15:04:55 +0300

    devtoolset9

--- devtools/devtools.sh
+++ devtools/devtools.sh
@@ -6,11 +6,11 @@ cd $(dirname $0)
 sudo cp nx_devtools.repo /etc/yum.repos.d/
 sudo cp nx_scl.repo /etc/yum.repos.d/
 
-sudo yum install -y devtoolset-8 \
-    devtoolset-8-libasan-devel \
-    devtoolset-8-libtsan-devel \
+sudo yum install -y devtoolset-9 \
+    devtoolset-9-libasan-devel \
+    devtoolset-9-libtsan-devel \
     llvm-toolset-9.0 \
     llvm-toolset-9.0-clang-devel \
-    llvm-toolset-9.0-llvm-devel
+    llvm-toolset-9.0-llvm-devel \
     llvm-toolset-9.0-cmake
 
