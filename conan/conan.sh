1340b08bac1c8beb481585fc755bb89c02d0da34
Alexander.Klishin <alexander.klishin@billing.ru>
2021-10-17 07:32:12 +0300

    conan lear

--- conan/conan.sh
+++ conan/conan.sh
@@ -8,8 +8,7 @@ virtualenv ~/conan
 . ~/conan/bin/activate
 
 pip3 install conan
-conan remote remove conan-center
-conan remote remove conancenter
+conan remote clear
 conan remote add arti \
       https://artifactory.nexign.com/artifactory/api/conan/conan-nwm-dev-local
 #conan user -r arti Alexander.Klishin -p XXXX
