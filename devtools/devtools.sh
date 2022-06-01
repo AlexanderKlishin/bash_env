8193851b1fdff83667de5e4dbfa920ebcea3f687
Alexander.Klishin <alexander.klishin@billing.ru>
2022-06-01 13:11:10 +0300

    Revert "devtoolset 11"
    This reverts commit fd09a632f32ea9e3a694d08c11295d7ec9008fbf.

--- devtools/devtools.sh
+++ devtools/devtools.sh
@@ -12,11 +12,5 @@ sudo yum install -y \
     devtoolset-10-libtsan-devel \
     llvm-toolset-10.0 \
     llvm-toolset-10.0-clang-devel \
-    llvm-toolset-10.0-llvm-devel \
-    devtoolset-11 \
-    devtoolset-11-libasan-devel \
-    devtoolset-11-libtsan-devel \
-    llvm-toolset-11.0 \
-    llvm-toolset-11.0-clang-devel \
-    llvm-toolset-11.0-llvm-devel
+    llvm-toolset-10.0-llvm-devel
 
