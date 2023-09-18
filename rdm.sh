336e3b9c56a285f6605781d4ca63a3ca61d76bb5
aklishin <aklishin@inno.tech>
2023-09-18 18:23:36 +0700

    rdm jobs = 6

--- rdm.sh
+++ rdm.sh
@@ -7,4 +7,5 @@ rm -rf ~/src/rdm ~/src/rdm.log
 
 #LD_LIBRARY_PATH=/opt/rh/llvm-toolset-7.0/root/usr/lib64 \
 rdm --daemon --allow-Werror --error-limit 1000 \
-    --data-dir ~/src/rdm --log-file ~/src/rdm.log
+    --data-dir ~/src/rdm --log-file ~/src/rdm.log \
+    -j 6
